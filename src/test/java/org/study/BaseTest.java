a4d51ea4a7593d76f34fe49aee33685f21473e26
fanqie <gyx645378029@163.com>
2020-01-05 20:22:45 +0800

    修复BaseTest类中无单测导致无法打包报错的问题

--- src/test/java/org/study/BaseTest.java
+++ src/test/java/org/study/BaseTest.java
@@ -1,5 +1,6 @@
 package org.study;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,4 +14,8 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @SpringBootTest
 @WebAppConfiguration
 public class BaseTest {
+
+    @Test
+    public void test() {
+    }
 }
