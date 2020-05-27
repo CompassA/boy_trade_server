abf78954b504b25e16835a16acdcf94663fe286e
番茄 <gyx645378029@163.com>
2020-05-27 16:49:37 +0800

    log aspect

---
+++ src/main/java/org/study/aspects/HttpLogAspect.java
@@ -0,0 +1,42 @@
+package org.study.aspects;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author fanqie
+ * @date 2020/5/27
+ */
+@Aspect
+@Component
+public class HttpLogAspect {
+
+    @Pointcut("execution(public * org.study.controller.*.*(..))")
+    public void log() {
+    }
+
+    @Around("log()")
+    public Object controllerAround(final ProceedingJoinPoint proceedingJoinPoint) {
+        final Signature signature = proceedingJoinPoint.getSignature();
+        final String controllerInfo = String.format("Controller Name: %s, Method Name: %s",
+                signature.getDeclaringTypeName(), signature.getName());
+        System.out.printf("Accept Request, %s\n", controllerInfo);
+
+        Object returnValue = null;
+        final long startTime = System.currentTimeMillis();
+        try {
+            returnValue = proceedingJoinPoint.proceed();
+        } catch (final Throwable throwable) {
+            throwable.printStackTrace();
+        } finally {
+            final long endTime = System.currentTimeMillis();
+            System.out.printf("End of request processing, %s, cost: %dms\n",
+                    controllerInfo, endTime - startTime);
+        }
+        return returnValue;
+    }
+}
